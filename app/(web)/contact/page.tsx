8ca7d0612edeacebf6c36ccd423b470bd1e2f162
Anees <aneespengad4447@gmail.com>
2026-08-06 11:02:17 +0530

    feat(seo): add SEO metadata for contact page

--- app/(web)/contact/page.tsx
+++ app/(web)/contact/page.tsx
@@ -1,8 +1,41 @@
-
 import LocationMap from "@/app/components/contact/LocationMap";
 import GetInTouch from "../../components/contact/ContactForm";
 import HeroContactUs from "../../components/contact/OurContact";
 import Navbar from "../../components/layout/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Contact Us | Wood World Decor LLC",
+    template: "%s | Wood World Decor LLC",
+  },
+  description:
+    "Get in touch with Wood World Decor LLC for expert joinery, fit-out in Dubai. Call +971 52 787 5262 or fill out our contact form for a free consultation",
+  keywords: "contact joinery Dubai",
+  metadataBase: new URL("https://www.wwduae.com"),
+  alternates: {
+    canonical: "/contact",
+  },
+  openGraph: {
+    title: "Contact Us | Wood World Decor LLC | Joinery & Fitout Dubai",
+    description:
+      "Get in touch with Wood World Decor LLC for expert renovation services in Dubai.Call +971 52 787 5262 or fill out our contact form for a free consultation",
+    url: "https://www.wwduae.com/contact",
+    siteName: "Wood World Decor LLC",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Contact Us | Wood World Decor LLC | Joinery & Fitout Dubai",
+    description:
+      "Get in touch with Wood World Decor LLC for expert joinery, fit-out, and renovation services in Dubai.",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default function Contact() {
   return (
