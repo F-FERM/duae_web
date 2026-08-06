34421f38be4266f1939c16ae99cdfb9971626953
Anees <aneespengad4447@gmail.com>
2026-08-06 10:59:47 +0530

    feat(seo): add metadata for blogs page

--- app/(web)/blogs/page.tsx
+++ app/(web)/blogs/page.tsx
@@ -1,6 +1,40 @@
+import { Metadata } from "next";
 import Blogs from "../../components/blog/Blogs";
 import HeroOurblogs from "../../components/blog/OurBlogs";
 import Navbar from "../../components/layout/Navbar";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Blogs | Wood World Decor LLC",
+    template: "%s | Wood World Decor LLC",
+  },
+  description:
+    "Read our latest blogs on joinery, fit-out solutions, and interior design trends in Dubai.Expert insights from Wood World Decor LLC for homes & retail spaces.",
+  keywords: "joinery blog Dubai",
+  metadataBase: new URL("https://www.wwduae.com"),
+  alternates: {
+    canonical: "/blogs",
+  },
+  openGraph: {
+    title: "Blogs | Wood World Decor LLC | Joinery & Fitout Insights Dubai",
+    description:
+      "Read our latest blogs on joinery, fit-out solutions, and interior design trends in Dubai.Expert insights from Wood World Decor LLC for homes, & retail spaces.",
+    url: "https://www.wwduae.com/blogs",
+    siteName: "Wood World Decor LLC",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Blogs | Wood World Decor LLC | Joinery & Fitout Insights Dubai",
+    description:
+      "Read our latest blogs on joinery, fit-out solutions, and interior design trends in Dubai. Expert insights from Wood World Decor LLC.",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default function Blog() {
   return (
